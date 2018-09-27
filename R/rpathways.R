f52709ce545fc3c42756b7288bf7f3c93b85d6e9
ernestguevarra <ernestgmd@gmail.com>
2018-09-27 13:29:48 +0100

    create rpathways.R

---
+++ R/rpathways.R
@@ -0,0 +1,29 @@
+################################################################################
+#
+#' rpathways
+#'
+#' An Implementation of the Results Pathways Monitoring in R
+#'
+#' Results pathways are usually based on a theory of change describing
+#' how change is assumed to come about through intervention/s in a prevailing
+#' situation. The theory is laid out in a diagram showing the connections
+#' between interventions and outcomes - the results pathways. From an
+#' epidemiological perspective, these results pathways can be conceived as
+#' directed acyclic graphs (DAG) more specifically those concerned with
+#' structural causal models that portray causal assumptions about a set of
+#' variables. Causal DAGs are consistent and easy to understand. Thus, when
+#' assessing the causal effect between an intervention and an outcome, drawing
+#' assumptions in the form of a DAG can help in picking the right model without
+#' necessarily having the mathematical basis behind it. Another way to think
+#' about DAGs is as non-parametric structural equation models (SEM) which
+#' explicitly lays out paths between variables. In the case of a DAG, the
+#' exact relationship between the two variables is not as important, only its
+#' direction. The rules underpinning DAGs are consistent whether the
+#' relationship is da simple, linear one, or a more complicated function.
+#'
+#' @docType package
+#' @name rpathways
+#'
+#
+################################################################################
+NULL
